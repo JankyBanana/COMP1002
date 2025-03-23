cb85ec1b4c556e013ccc2b025fdb0245274d1b74
lukel <luke.lummis@hotmail.com>
2025-03-23 22:00:52 +0800

    Added exception handling to each function

--- Practical_2/RecursiveFunctions.py
+++ Practical_2/RecursiveFunctions.py
@@ -3,11 +3,11 @@
 #
 
 def iterFibonacci(n):
-    fibVal = 0
     currVal = 1
     lastVal = 0
-
-    if (n == 0):
+    if n < 0:
+        raise ValueError("Imput must not be negative")
+    elif (n == 0):
         fibVal = 0
     elif (n == 1):
         fibVal = 1
@@ -19,7 +19,9 @@ def iterFibonacci(n):
     return fibVal
 
 def recurFibonacci(n):
-    if (n == 0):
+    if n < 0:
+        raise ValueError("Import must not be negative")
+    elif (n == 0):
         fibVal = 0
     elif (n == 1):
         fibVal = 1
@@ -28,24 +30,42 @@ def recurFibonacci(n):
     return fibVal
 
 def iterFactorial(n):
+    if n < 0:
+        raise ValueError("Import must not be negative")
     val = 1
     for i in range(1, n+1):
         val *= i
     return val
 
 def recurFactorial(n):
+    if n < 0:
+        raise ValueError("Import must not be negative")
     if n == 0 or n == 1:
         val = 1
     else:
         val = n * recurFactorial(n-1)
     return val
 
-def GCD(n):
+def GCD(x, y):
+    if x < 0 or y < 0:
+        raise ValueError("Import must not be negative")
+    if y == 0:
+        return x
+    else:
+        return GCD(y, x % y)
+# GCD function was found on:
+# https://stackoverflow.com/questions/59147282/how-to-find-greatest-common-divisor-using-recursive-function-in-python
 
-
+def DecToBin(n):
+    if n < 2:
+        return str(n)
+    else:
+        return DecToBin(n // 2) + str(n % 2)
+#DecToBin function was found on:
+#https://docs.vultr.com/python/examples/convert-decimal-to-binary-using-recursion
 
 def main():
-    x = 9
+    x = 10
     print(f'Fibonacci number {x} is: {iterFibonacci(x)}, {recurFibonacci(x)}')
     print(f'Factorial number {x} is: {iterFactorial(x)}, {recurFactorial(x)}, \n')
 main()
