466ec76fee92a91fc629c221940c6c6db02d3bfd
JankyBanana <luke.lummis@hotmail.com>
2025-03-27 12:32:59 +0800

    Changed DecToBin function to DecToBaseN to convert any decimal/base 10 number into bases 2 to 16.

--- Practical_2/RecursiveFunctions.py
+++ Practical_2/RecursiveFunctions.py
@@ -2,70 +2,87 @@
 # A set of functions with recursive behaviour and their iterative version
 #
 
-def iterFibonacci(n):
+def iterFibonacci(number):
     currVal = 1
     lastVal = 0
-    if n < 0:
+    if number < 0:
         raise ValueError("Imput must not be negative")
-    elif (n == 0):
+    elif (number == 0):
         fibVal = 0
-    elif (n == 1):
+    elif (number == 1):
         fibVal = 1
     else:
-        for ii in range(2, n + 1):
+        for ii in range(2, number + 1):
             fibVal = currVal + lastVal
             lastVal = currVal
             currVal = fibVal
     return fibVal
 
-def recurFibonacci(n):
-    if n < 0:
+def recurFibonacci(number):
+    if number < 0:
         raise ValueError("Import must not be negative")
-    elif (n == 0):
+    elif (number == 0):
         fibVal = 0
-    elif (n == 1):
+    elif (number == 1):
         fibVal = 1
     else:
-        fibVal = recurFibonacci(n-1) + recurFibonacci(n-2)
+        fibVal = recurFibonacci(number-1) + recurFibonacci(number-2)
     return fibVal
 
-def iterFactorial(n):
-    if n < 0:
+def iterFactorial(number):
+    if number < 0:
         raise ValueError("Import must not be negative")
     val = 1
-    for i in range(1, n+1):
+    for i in range(1, number+1):
         val *= i
     return val
 
-def recurFactorial(n):
-    if n < 0:
+def recurFactorial(number):
+    if number < 0:
         raise ValueError("Import must not be negative")
-    if n == 0 or n == 1:
+    if number == 0 or number == 1:
         val = 1
     else:
-        val = n * recurFactorial(n-1)
+        val = number * recurFactorial(number-1)
     return val
 
-def GCD(x, y):
-    if x < 0 or y < 0:
+def GCD(number1, number2):
+    if number1 < 0 or number2 < 0:
         raise ValueError("Import must not be negative")
-    if y == 0:
-        return x
+    elif number2 == 0:
+        return number1
     else:
-        return GCD(y, x % y)
-# GCD function was found on:
+        return GCD(number2, number1 % number2)
+# GCD function was made using:
 # https://stackoverflow.com/questions/59147282/how-to-find-greatest-common-divisor-using-recursive-function-in-python
 
-def DecToBin(n):
-    if n < 2:
-        return str(n)
-    else:
-        return DecToBin(n // 2) + str(n % 2)
-#DecToBin function was found on:
-#https://docs.vultr.com/python/examples/convert-decimal-to-binary-using-recursion
+def DecToBaseN(decimal, base):
+    baseList = "0123456789ABCDEF"
+    try:
+        if base < 2:
+            raise ValueError("Base 1 doesn't exist, valid range is from 2 to 16.")
+        elif base > 16:
+            raise ValueError("Outside the range of valid bases, valid range is from base 2 to 16 ")
+        elif decimal < base:
+            return str(baseList[decimal])
+        else:
+            return DecToBaseN(decimal // base, base) + str(baseList[(decimal % base)])
+    except ValueError as err:
+        print(err)
+
 
 def main():
-    x = 10
-    print(f'Fibonacci number {x} is: {iterFibonacci(x)}, {recurFibonacci(x)}')
-    print(f'Factorial number {x} is: {iterFactorial(x)}, {recurFactorial(x)}, \n')
+    number1 = 1
+    print(f'\nFibonacci number {number1} is: {iterFibonacci(number1)}, {recurFibonacci(number1)}')
+    print(f'The factorial of {number1} or {number1}! is: {iterFactorial(number1)}, {recurFactorial(number1)}\n')
+
+
+    number2 = 1440
+    number3 = 140
+    print(f'The greatest common denominator of {number2} and {number3} is: {GCD(number2, number3)}\n')
+
+    decimal = 3223481
+    base = 16
+    print(f'The Base 10 number {decimal} converted to Base {base} is: {DecToBaseN(decimal, base)}\n')
+
 main()
