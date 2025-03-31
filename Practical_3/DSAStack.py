fc1587a0e1e845d62255a3d3cc8030e0fac887b1
JankyBanana <luke.lummis@hotmail.com>
2025-03-31 14:16:21 +0800

    Implemented the class Stack in DSAStack.py with the methods required by practical 3

---
+++ Practical_3/DSAStack.py
@@ -0,0 +1,51 @@
+#
+# Implementation of the DSAStack abstract
+# data type for COMP1002 practical 3
+#
+from itertools import count
+
+import numpy as np
+
+class Stack():
+    defaultSize = 100
+
+    def __init__(self, name):
+        self.stackArray = np.full(Stack.defaultSize, None)
+        self.numElements = 0
+        self.size = len(self.stackArray)
+        self.name = name
+
+    def push(self, value):
+        if self.isFull == True:
+            raise MemoryError(f"New item can't be added to stack, {self.name} as it is full")
+        else:
+            self.stackArray[self.numElements] = value
+            self.numElements += 1
+
+    def pop(self):
+        if self.isEmpty == True:
+            raise IndexError(f"Can't access top item as stack, {self.name} is empty")
+        else:
+            stackTop = self.stackArray[self.numElements-1]
+            self.stackArray[self.numElements-1] = None
+            self.numElements -= 1
+            return stackTop
+
+    def peek(self):
+        if self.isEmpty == True:
+            raise IndexError(f"Can't access top item as stack, {self.name} is empty")
+        else:
+            stackTop = self.stackArray[self.numElements-1]
+            return stackTop
+
+    def isEmpty(self):
+        if self.numElements == 0:
+            return True
+        else:
+            return False
+
+    def isFull(self):
+        if self.numElements == self.size:
+            return True
+        else:
+            return False
