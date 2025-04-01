d1782cbeea902f6a6845f1b440cb5b3bcdc2e364
JankyBanana <luke.lummis@hotmail.com>
2025-04-01 17:00:32 +0800

    Implemented the infixToPostfix function

---
+++ Practical_3/EquationSolver.py
@@ -0,0 +1,42 @@
+#
+# A program that takes the string representation
+# of a mathematical equation in infix form and solves
+# it by converting it to postfix then evaluating the postfix
+#
+
+import DSAStack as DSAS
+
+def infixToPostfix():
+    opList = {"+":0, "-":0,  "*":1,  "/":1}
+    opStack = DSAS.DSAStack("operators", 20)
+    postfix = ""
+
+    equation = input("Enter an equation in infix form: ")
+    for i in range(len(equation)):
+        term = equation[i]
+        if term == "(":
+            opStack.push(term)
+        elif term == ")":
+            while opStack.peek() != "(":
+                postfix = postfix + " " + opStack.pop()
+            opStack.pop()
+        elif term in opList:
+            while (opStack.isEmpty() == False and opStack.peek() != "(" and
+                   opList[opStack.peek()] >= opList[term]):
+                postfix = postfix + " " + opStack.pop()
+            opStack.push(term)
+        else:
+            postfix = postfix + " " + str(term)
+
+    while opStack.isEmpty() == False:
+        postfix = postfix + str(opStack.pop())
+    print(postfix)
+
+#def solvePostfix(equation):
+
+
+#def solve(equation):
+
+def main():
+    infixToPostfix()
+main()
