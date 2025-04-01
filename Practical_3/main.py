faa8db554d0d7efdcb61c4d653879bdc97eda811
JankyBanana <luke.lummis@hotmail.com>
2025-04-01 14:29:11 +0800

    Implemented the stackTest() function

--- Practical_3/main.py
+++ Practical_3/main.py
@@ -5,7 +5,47 @@
 import DSAStack as DSAS
 import DSAQueue as DSAQ
 
-def main():
+def stackTest():
+    # Demonstration of the DSAStack class in DSAStack.py
+    stack = DSAS.DSAStack("stack", 10)
+    print(f"Stack is:\n{stack.stackArray}\n")
 
+    # Full or empty stack check
+    print(f"The stack is empty? {stack.isEmpty()}")
+    print(f"The stack is full? {stack.isFull()}\n")
+
+    # Pushing one-less than max items to the stack
+    print("Adding none-less than max items in ascending order to the stack")
+    for i in range(stack.size - 1):
+        stack.push(i + 1)
+    print(f"Stack is:\n{stack.stackArray}\n")
+
+    # Full or empty stack check
+    print(f"The stack is empty? {stack.isEmpty()}")
+    print(f"The stack is full? {stack.isFull()}\n")
+
+    # Pushing one more item to the stack
+    print("Pushing 10 to the stack")
+    stack.push(10)
+    print(f"Stack is:\n{stack.stackArray}\n")
+
+    # Full or empty stack check
+    print(f"The stack is empty? {stack.isEmpty()}")
+    print(f"The stack is full? {stack.isFull()}\n")
+
+    # Peeking the top item of the stack
+    print(f"Taking a peek at the top item of the stack\n"
+          f"Top item = {stack.peek()}")
+    print(f"Stack is: {stack.stackArray}\n")
+
+    # Popping the top item off the stack
+    print(f"Taking the top item from the stack\n"
+          f"Taken item = {stack.pop()}")
+    print(f"Stack is:\n{stack.stackArray}\n")
+
+
+def main():
+    stackTest()
+    #shufflingQueueTest()
 main()
 
