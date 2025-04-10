a2e941fab00e1f5178cfa61e419742172c191231
JankyBanana <luke.lummis@hotmail.com>
2025-04-10 15:19:34 +0800

    Altered the stackTest() to account for DSAStack using linked lists instead of arrays

--- Practical_4/StackQueueTest.py
+++ Practical_4/StackQueueTest.py
@@ -7,41 +7,38 @@ import DSAQueue as DSAQ
 
 def stackTest():
     print("Demonstration of the DSAStack class in DSAStack.py")
-    stack = DSAS.DSAStack("stack", 10)
-    print(f"Stack is:\n{stack.stackArray}\n")
+    stack = DSAS.DSAStack("stack")
+    stack.stackContents()
 
     # Full or empty stack check
     print(f"The stack is empty? {stack.isEmpty()}")
-    print(f"The stack is full? {stack.isFull()}\n")
 
-    # Pushing one-less than max items to the stack
-    print("Adding one-less than max items in ascending order to the stack")
-    for i in range(stack.size - 1):
-        stack.push(i + 1)
-    print(f"Stack is:\n{stack.stackArray}\n")
+    # Pushing 10 items to the stack
+    print("Adding 10 items in ascending order to the stack")
+    for i in range(10):
+        stack.push(i)
+    stack.stackContents()
 
     # Full or empty stack check
     print(f"The stack is empty? {stack.isEmpty()}")
-    print(f"The stack is full? {stack.isFull()}\n")
 
     # Pushing one more item to the stack
     print("Pushing 10 to the stack")
     stack.push(10)
-    print(f"Stack is:\n{stack.stackArray}\n")
+    stack.stackContents()
 
     # Full or empty stack check
     print(f"The stack is empty? {stack.isEmpty()}")
-    print(f"The stack is full? {stack.isFull()}\n")
 
     # Peeking the top item of the stack
     print(f"Taking a peek at the top item of the stack\n"
           f"Top item = {stack.peek()}")
-    print(f"Stack is: {stack.stackArray}\n")
+    stack.stackContents()
 
     # Popping the top item off the stack
     print(f"Taking the top item from the stack\n"
           f"Taken item = {stack.pop()}")
-    print(f"Stack is:\n{stack.stackArray}\n")
+    stack.stackContents()
 
 def shufflingQueueTest():
     print("Demonstration of the ShufflingQueue subclass in DSAQueue.py")
@@ -137,8 +134,8 @@ def circularQueueTest():
 def main():
     stackTest()
     print("-"*65, "\n")
-    shufflingQueueTest()
-    print("-" * 65, "\n")
-    circularQueueTest()
+    # shufflingQueueTest()
+    # print("-" * 65, "\n")
+    # circularQueueTest()
 main()
 
