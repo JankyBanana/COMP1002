8e95c7cb4a606d993cd63602b5169805723466a5
JankyBanana <luke.lummis@hotmail.com>
2025-04-10 17:28:01 +0800

    Modified both shuffling and circular queues to use linked lists instead of arrays

--- Practical_4/DSAQueue.py
+++ Practical_4/DSAQueue.py
@@ -2,15 +2,15 @@
 # Implementation of the shuffling circular DSAQueue abstract
 # data type using a linked list for practical 4
 #
+# Derived from DSAQueue.py submitted COMP1002 Practical 3
+#
 
-import numpy as np
+import LinkedList as ll
 
 class DSAQueue():
-    defaultSize = 100
 
-    def __init__(self, name, size = defaultSize):
-        self.size = size
-        self.queueArray = np.full(self.size, None, dtype=object)
+    def __init__(self, name):
+        self.linkedList = ll.DSALinkedList(name)
         self.numElements = 0
         self.name = name
 
@@ -24,68 +24,39 @@ class DSAQueue():
         raise NotImplementedError("peek must be implemented in subclass")
 
     def isEmpty(self):
-        if self.numElements == 0:
-            return True
-        else:
-            return False
+        return self.linkedList.isEmpty()
 
-    def isFull(self):
-        if self.numElements == self.size:
-            return True
-        else:
-            return False
+    def queueContents(self):
+        return self.linkedList.printNodeValues()
 
 class ShufflingQueue(DSAQueue):
     def enqueue(self, value):
-        if self.isFull():  # Same as saying if self.isFull == True
-            raise MemoryError(f"Can't add new item as the queue, {self.name} is full")
-        else:
-            self.queueArray[self.numElements] = value
-            self.numElements += 1
+        self.linkedList.insertLast(value)
+        self.numElements += 1
 
     def dequeue(self):
-        if self.isEmpty():
-            raise IndexError(f"Can't dequeue next item as the queue, {self.name} is empty")
-        else:
-            queueNext = self.queueArray[0]
-            for i in range(1, self.numElements):
-                self.queueArray[i-1] = self.queueArray[i]
-            self.queueArray[self.numElements-1] = None
-            self.numElements -= 1
-            return queueNext
+        return self.linkedList.removeFirst()
 
     def peek(self):
-        if self.isEmpty() == True:
-            raise IndexError(f"Can't access next item as the queue, {self.name} is empty")
-        else:
-            return self.queueArray[0]
+        return self.linkedList.peekFirst()
 
 class CircularQueue(DSAQueue):
-    def __init__(self, name, size):
-        super().__init__(name, size)
-        self.queueEnd = 0
-        self.queueStart = 0
-
     def enqueue(self, value):
-        if self.isFull():
-            raise MemoryError(f"Can't add new item as the queue, {self.name} is full")
+        self.linkedList.insertLast(value)
+        if self.isEmpty():
+            self.linkedList.tail.next = self.linkedList.tail
+            self.linkedList.tail.prev = self.linkedList.head
         else:
-            self.queueArray[self.queueEnd] = value
-            self.queueEnd = (self.queueEnd+1) % self.size
-            self.numElements += 1
+            self.linkedList.head.prev = self.linkedList.tail
+            self.linkedList.tail.next = self.linkedList.head
+        self.numElements += 1
 
     def dequeue(self):
-        if self.isEmpty() == True:
-            raise IndexError(f"Can't dequeue next item as the queue, {self.name} is empty")
-        else:
-            queueNext = self.queueArray[self.queueStart]
-            self.queueArray[self.queueStart] = None
-            self.queueStart = (self.queueStart + 1) % self.size
-            self.numElements -= 1
-            return queueNext
+        queueNext = self.linkedList.removeFirst()
+        self.linkedList.head.prev = self.linkedList.tail
+        self.linkedList.tail.next = self.linkedList.head
+        self.numElements -= 1
+        return queueNext
 
     def peek(self):
-        if self.isEmpty() == True:
-            raise IndexError(f"Can't access next item as the queue, {self.name} is empty")
-        else:
-            return self.queueArray[self.queueStart]
+        return self.linkedList.peekFirst()
