d6ca0171c2b17abfa854f2d97a2266313a6b6330
lukel <luke.lummis@hotmail.com>
2025-04-16 21:57:09 +0800

    Created BST and BSTNode classes

--- Practical_5/BinaryTree.py
+++ Practical_5/BinaryTree.py
@@ -2,4 +2,26 @@
 # Implementation of the Binary Search Tree data structure for practical 5
 #
 
-class DSABinarySearchTree()
+class DSABinarySearchTree():
+    class _DSATreeNode():
+        def __init__(self, key, data):
+            self._key = key
+            self._data = data
+            self._left = None
+            self._right = None
+
+        def __str__(self):
+            print(f"Key: {self._key}  Data: {self._data}")
+
+    def __init__(self):
+        self.root = None
+
+    #def find(self):
+
+    #def insert(self, key, value):
+
+    #def delete(self, key):
+
+    #def display(self):
+
+    #def height(self):
