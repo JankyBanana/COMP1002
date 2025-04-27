b8ef8ed8de089b45eaf8cb172f81aecf49623c64
lukel <luke.lummis@hotmail.com>
2025-04-27 17:58:15 +0800

    Implemented bases for methods required for Graph class

--- Practical_6/Graph.py
+++ Practical_6/Graph.py
@@ -8,12 +8,14 @@ import LinkedList as LL
 
 class DSAGraph():
     #----------- Private inner GraphNode class -----------#
-    class _DSAGraphNode():
+    class _DSAGraphVertex():
         def __init__(self, _label: str=None, _data=None):
             self._label = _label
             self._data = _data
             self._connections = LL.DSALinkedList()
             self._visited = False
+            self._outDegree = 0
+            self._inDegree = 0
 
         def _getLabel(self):
             return self._label
@@ -32,8 +34,9 @@ class DSAGraph():
                 connectionList.insertLast(self._label + tempList.removeFirst())
             return connectionList
 
-        def _addConnection(self, edge: str):
-            self._connections.insertLast(edge[1])
+        def _addEdge(self, sink: str):
+            self._inDegree += 1
+            self._connections.insertLast(sink)
 
         def setVisited(self):
             self._visited = True
@@ -44,4 +47,70 @@ class DSAGraph():
         def getVisited(self):
             return self._visited
 
-    #----------- Graph class -----------#
+    #--------------------- Graph class ---------------------#
+    def __init__(self):
+        self.vertices = LL.DSALinkedList()
+
+    def vertexFind(self, label: str):
+        tempList = self.vertices
+
+        for i in range(self.vertices.nodes):
+            if tempList.removeFirst()._getLabel == label:
+                return True
+        return False
+
+    def addVertex(self, label: str, data=None):
+        if self.vertexFind(label):
+            raise ValueError("Vertex label already in use")
+
+        self.vertices.insertLast(self._DSAGraphVertex(label, data))
+
+    def addEdge(self, sourceLabel: str, sinkLabel: str):
+        sourceVertex = self.getVertex(sourceLabel)
+
+        if self.vertexFind(sourceLabel) == False:
+            raise Exception("Source vertex not found")
+        elif sourceVertex._connections.find(sinkLabel):
+            raise Exception("Edge between these vertices already exists")
+
+        self.getVertex(sourceLabel)._addEdge(sinkLabel)
+        self.getVertex(sinkLabel)._outDegree += 1
+
+    def getVertex(self, label: str):
+        tempList = self.vertices
+
+        for i in range(self.vertices.nodes):
+            if tempList.peekFirst()._getLabel == label:
+                return tempList.peekFirst()
+            tempList.removeFirst()
+        raise Exception("Vertex label not found")
+
+    def getVertexCount(self):
+        return self.vertices.nodes
+
+    def getEdgeCount(self):
+        sumOutDegree = 0
+        tempList = self.vertices
+
+        for i in range(self.vertices.nodes):
+            sumOutDegree += tempList.removeFirst()._outDegree
+
+        return sumOutDegree
+
+    def getAdjacentTo(self):
+        pass
+
+    def getAdjacentFrom(self):
+        pass
+
+    def isAdjacentTo(self):
+        pass
+
+    def isAdjacentFrom(self):
+        pass
+
+    def displayAsList(self):
+        pass
+
+    def displayAsMatrix(self):
+        pass
