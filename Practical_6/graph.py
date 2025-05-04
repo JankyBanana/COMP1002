f132ecf21e73b20ca17159bea43eec39daa4b549
JankyBanana <luke.lummis@hotmail.com>
2025-05-04 15:23:32 +0800

    updated get_adjacent() method to pass onto graphVertex subclass method

--- Practical_6/graph.py
+++ Practical_6/graph.py
@@ -16,25 +16,8 @@ class DSAGraph:
             self.out_degree = 0
             self.edges = ll.DSALinkedList()
 
-        def _get_adjacent_to(self):
-            next_edge = self.edges.head
-            adjacent_list = ll.DSALinkedList()
-
-            while next_edge is not None:
-                if next_edge.data[0] == self.label:
-                    adjacent_list.insertLast(next_edge.data)
-                next_edge = next_edge.next
-            return adjacent_list
-
-        def _get_adjacent_from(self):
-            next_connection = self.edges.head
-            adjacent_list = ll.DSALinkedList()
-
-            while next_connection is not None:
-                if next_connection.data[1] == self.label:
-                    adjacent_list.insertLast(next_connection.data)
-                next_connection = next_connection.next
-            return adjacent_list
+        def _get_adjacent(self):
+            return self.edges
 
         def _add_edge(self, target_label: str, direction: str="o"):
             if direction == "o":
@@ -138,8 +121,7 @@ class DSAGraph:
 
     def get_adjacent(self, vertex_label: str):
         vertex = self.get_vertex(vertex_label)
-
-        return vertex.data.edges
+        return vertex.data._get_adjacent()
 
     def is_adjacent(self, source: str, sink: str):
         source_vertex = self.get_vertex(source)
