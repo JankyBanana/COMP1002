11094cd4fefc99b7103119b5e41fa0bd3f52a383
JankyBanana <luke.lummis@hotmail.com>
2025-05-12 13:42:34 +0800

    Implemented DSAHashEntry companion class

--- Practical_7/hashtable.py
+++ Practical_7/hashtable.py
@@ -1,13 +1,10 @@
 #
-# Implementation of the hash table data structure for COMP1002 practical 7
+# Implementation of the hash table values structure for COMP1002 practical 7
 #
 
 
-import numpy as np
-
-
 def next_prime(start_value):
-    start_value = int(start_value)+1
+    start_value = int(start_value) + 1
     if start_value % 2 == 0:
         prime_value = start_value - 1
     else:
@@ -30,5 +27,55 @@ def next_prime(start_value):
 
 
 class DSAHashTable:
+    def __init__(self, init_size: int = 31):
+        self.init_size = init_size
+
+    def put(self):
+        pass
+
+    def get(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def has_key(self):
+        pass
+
+    def hash(self):
+        pass
+
+    def step_hash(self):
+        pass
+
+    def display(self):
+        pass
+
+    def load_factor(self):
+        pass
+
+    def resize(self):
+        pass
+
+    def _find(self):
+        pass
+
 
 class DSAHashEntry:
+    def __init__(self):
+        self.key: str = None
+        self.data: object = None
+        self.state = 0  # 0 = Never used, 1 = Currently used, -1 = Previously used
+
+    def _put(self, key: str, data: object):
+        self.key = key
+        self.data = data
+        self.state = 1
+
+    def _get(self):
+        return self.data
+
+    def _remove(self):
+        self.key: str = None
+        self.data: object = None
+        self.state = -1
