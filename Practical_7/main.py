973af94434c3f048c8f0730d1f004b1445df4d36
JankyBanana <luke.lummis@hotmail.com>
2025-05-12 18:07:54 +0800

    Added testing for hash table class

--- Practical_7/main.py
+++ Practical_7/main.py
@@ -1,30 +1,27 @@
 #
-# Main program section for practical 7
+# Main program file for practical 7
 #
 
 
-import DSAHashTable as HT
+import hashtable as ht
 
-demo = HT.HashTable(31)
+demo = ht.DSAHashTable()
 demo.display()
-print('\n')
 
 for i in range(31):
-    print(f"Inserting key: {i}, Value: {demo.hash(str(i))}")
+    print(f"Inserting key: {i}, Data: {i}, Hash Index: {demo.hash(str(i))}")
     demo.put(str(i), i)
 print('\n')
 
 demo.display()
+
+print(f"Getting 27 gives: {demo.get('27')}")
 print('\n')
 
-print(f"Does demo have key 25? {demo.has_key("25")}")
-print(f"Does demo have key 25? {demo.has_key("32")}")
-print('\n')
-
-a = demo.find("11")
-print(f"Key of a: {a.key}, Value of a: {a.value}")
-print('\n')
-
-demo.remove('11')
 demo.display()
-print("\nValue of key 12 is:", demo.get("12"))
+
+demo.remove('24')
+demo.display()
+
+print(f"Does demo have the key '23'? {demo.has_key('23')}")
+print(f"Does demo have the key '31'? {demo.has_key('31')}")
