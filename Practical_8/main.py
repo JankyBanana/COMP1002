5e060763dd0a6fc6d24114e11461b5f9d86bc341
JankyBanana <luke.lummis@hotmail.com>
2025-05-14 19:12:22 +0800

    Added int_to_chr() function for testing

--- Practical_8/main.py
+++ Practical_8/main.py
@@ -6,4 +6,16 @@
 import heap
 
 
-teat_heap = heap.DSAHeap()
+def int_to_char(num):
+    if 0 < num < 27:
+        return chr(num + 96)
+
+
+x = heap.DSAHeap(size=30)
+
+for i in range(1, 27):
+    x.add(i, int_to_char(i))
+
+    for i in range(x.count):
+        print(i, x.heap_array[i].priority, x.heap_array[i].data)
+    print('\n')
