d0d27ece634635d51ba8bc8ef71e2ce9c8b5f0ca
JankyBanana <luke.lummis@hotmail.com>
2025-05-15 12:10:31 +0800

    Now saves the csv_array, initial csv_heap from adding csv_array and the final heapSorted heap

--- Practical_8/main.py
+++ Practical_8/main.py
@@ -3,60 +3,60 @@
 #
 
 
-import heap
 import numpy as np
 
-from Practical_8.heap import DSAHeap, DSAHeapEntry
+import heap as h
 
-csv_file = open('RandomNames7000.csv', 'r')
-array = np.full(7000,None, dtype=object)
+with open('RandomNames7000.csv', 'r') as csv_file:
+    csv_array = np.full(7000, None, dtype=object)
 
-for x in range(7000):
-    line = csv_file.readline()
+    for x in range(7000):
+        line = csv_file.readline()
 
-    priority = ''
-    data = ''
-    comma_index = 0
-    comma_found = False
+        priority = ''
+        data = ''
+        comma_index = 0
+        comma_found = False
 
-    for i in range(len(line)):
-        if line[i] == ',':
-            comma_found = True
-            comma_index = i
-            break
+        for i in range(len(line)):
+            if line[i] == ',':
+                comma_found = True
+                comma_index = i
+                break
 
-    for i in range(comma_index):
-        priority += line[i]
-    priority = int(priority)
+        for i in range(comma_index):
+            priority += line[i]
 
-    for i in range(comma_index + 1, len(line) - 1):
-        data += line[i]
+        for i in range(comma_index + 1, len(line) - 1):
+            data += line[i]
 
-    array[x] = DSAHeapEntry(priority, data)
-csv_file.close()
+        csv_array[x] = h.DSAHeapEntry(int(priority), data)
 
-sorted_array = DSAHeap.heap_sort(array)
-
-with open("RandomNames7000_HeapSorted.txt", "w") as f:
+# saving csv array
+with open("csv_array.txt", "w") as f:
     txt_string = ''
 
-    for i in range(7000):
-        txt_string += f'{sorted_array[i].priority},{sorted_array[i].data}\n'
+    for entry in csv_array:
+        txt_string += f'{entry.priority},{entry.data}\n'
     f.write(txt_string)
-f.close()
 
-# Testing code for heap.py
-# def int_to_char(num):
-#     if 0 < num < 27:
-#         return chr(num + 96)
-#
-# x = heap.DSAHeap(size=30)
-#
-# for i in range(1, 27):
-#     x.add(i, int_to_char(i))
-#
-# x.display()
-#
-# a = x.remove()
-# x.display()
-# print(a.priority, a.priority)
+# creating csv heap from csv array and saving it
+csv_heap = h.DSAHeap(size=7000)
+for entry in csv_array:
+    csv_heap.add(entry.priority, entry.data)
+
+with open("csv_heap_raw.txt", "w") as f:
+    txt_string = ''
+
+    for entry in csv_heap.heap_array:
+        txt_string += f'{entry.priority},{entry.data}\n'
+    f.write(txt_string)
+
+# heapSorting and saving csv heap
+csv_heap.heap_sort()
+with open("RandomNames_HeapSort.txt", "w") as f:
+    txt_string = ''
+
+    for entry in csv_heap.heap_array:
+        txt_string += f'{entry.priority},{entry.data}\n'
+    f.write(txt_string)
