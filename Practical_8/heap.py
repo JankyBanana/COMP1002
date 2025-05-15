59528671dafc7d67d8c379072a39f455944bc15e
JankyBanana <luke.lummis@hotmail.com>
2025-05-15 12:09:39 +0800

    Added code demo and accounted for heap shrinkage when using heap_sort()

--- Practical_8/heap.py
+++ Practical_8/heap.py
@@ -6,6 +6,11 @@
 import numpy as np
 
 
+def int_to_char(num):
+    if 0 <= num < 27:
+        return chr(num + 97)
+
+
 class DSAHeapEntry:
     def __init__(self, priority: int, data: object = None):
         self.priority = priority
@@ -14,8 +19,8 @@ class DSAHeapEntry:
 
 class DSAHeap:
     def __init__(self, size: int = 100):
-        self.size = size
         self.heap_array = np.full(size, fill_value=None, dtype=object)
+        self.size = self.heap_array.size
         self.count = 0
 
     def add(self, priority: int, data: object):
@@ -37,6 +42,7 @@ class DSAHeap:
             top = self.heap_array[0]
             self.heap_array[0] = None
             self.count -= 1
+            print(f"Removed {top} = {top.priority} {top.data}")
             return top
         else:
             self.count -= 1
@@ -44,12 +50,14 @@ class DSAHeap:
             self.heap_array[0] = self.heap_array[self.count]
             self.heap_array[self.count] = None
             self.heap_array = self._trickle_down(0, self.heap_array)
+            print(f"Removed: [{top.priority} {top.data}]")
             return top
 
     def display(self):
-        print("Displaying heap array.")
+        print("Displaying heap array")
         for i in range(self.count):
             print(f"[Idx Priority Data]: {i} {self.heap_array[i].priority} {self.heap_array[i].data}")
+        print('')
 
     def _trickle_up(self, current_idx, heap_array):
         if current_idx > 0:
@@ -86,44 +94,28 @@ class DSAHeap:
         else:
             return heap_array
 
-    @staticmethod
-    def heapify(array):
-        count = len(array)
+    def heapify(self):
+        for i in range((self.count // 2) - 1, -1, -1):
+            self.heap_array = self._trickle_down(i, self.heap_array)
 
-        for i in range((count // 2) - 1, -1, -1):
-            array = DSAHeap.static_trickle_down(i, array, count)
-        return array
+    def heap_sort(self):
+        self.heapify()
+        original_count = self.count
 
-    @staticmethod
-    def heap_sort(array):
-        heap_array = DSAHeap.heapify(array)
+        for i in range(self.count - 1, 0, -1):
+            temp = self.heap_array[i]
+            self.heap_array[i] = self.heap_array[0]
+            self.heap_array[0] = temp
+            self.count = i
+            self._trickle_down(0, self.heap_array)
+        self.count = original_count
 
-        for i in range(array.size - 1, 0, -1):
-            temp = heap_array[i]
-            heap_array[i] = heap_array[0]
-            heap_array[0] = temp
-            heap_array = DSAHeap.static_trickle_down(0, heap_array, i)
-
-        return heap_array
-
-    @staticmethod
-    def static_trickle_down(current_idx, heap_array, count):
-        left_child = current_idx * 2 + 1
-        right_child = left_child + 1
-
-        if left_child < count:
-            bigger_child = left_child
-
-            if right_child < count:
-                if heap_array[left_child].priority < heap_array[right_child].priority:
-                    bigger_child = right_child
-
-            if heap_array[bigger_child].priority > heap_array[current_idx].priority:
-                temp = heap_array[bigger_child]
-                heap_array[bigger_child] = heap_array[current_idx]
-                heap_array[current_idx] = temp
-                return DSAHeap.static_trickle_down(bigger_child, heap_array, count)
-            else:
-                return heap_array
-        else:
-            return heap_array
+# print("----- Code Demo -----")
+# x = DSAHeap(size=30)
+#
+# for i in range(0, 5):
+#     x.add(i, int_to_char(i))
+# x.display()
+#
+# x.heap_sort()
+# x.display()
