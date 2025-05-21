0f419a28a492aff1796f0c6f50addc926aa3d54c
JankyBanana <luke.lummis@hotmail.com>
2025-05-21 15:57:17 +0800

    Implemented mergeSort() function

--- Practical_9/DSAsorts.py
+++ Practical_9/DSAsorts.py
@@ -6,6 +6,8 @@
 # Copied from Practical 1
 #
 
+import numpy as np
+
 def bubbleSort(A):
     for passes in range(A.size - 1):
         for i in range(A.size - passes - 1):
@@ -28,15 +30,48 @@ def selectionSort(A):
         A[min_index], A[p] = A[p], A[min_index]
 
 def mergeSort(A):
-    """ mergeSort - front-end for kick-starting the recursive algorithm
-    """
-    ...
+    left_index = 0
+    right_index = A.size
+    mergeSortRecurse(A, left_index, right_index)
 
-def mergeSortRecurse(A, leftIdx, rightIdx):
-    ...
+def mergeSortRecurse(A, left_index, right_index):
+    if left_index < right_index:
+        mid_index = (left_index + right_index) // 2
 
-def merge(A, leftIdx, midIdx, rightIdx):
-    ...
+        mergeSortRecurse(A, left_index, mid_index)
+        mergeSortRecurse(A, mid_index + 1, right_index)
+
+        merge(A, left_index, mid_index, right_index)
+    return A
+
+def merge(A, left_index, mid_index, right_index):
+    temp_size = right_index - left_index + 1
+    temp_array = np.zeros(temp_size)
+    ii = left_index
+    jj = mid_index + 1
+    kk = 0
+
+    while ii <= mid_index and jj <= right_index:
+        if A[ii] <= A[jj]:
+            temp_array[kk] = A[ii]
+            ii += 1
+        else:
+            temp_array[kk] = A[jj]
+            jj += 1
+        kk += 1
+
+    for ii in range(ii, mid_index):
+        temp_array[kk] = A[ii]
+        kk += 1
+
+    for jj in range(jj, jj + 1):
+        temp_array[kk] = A[jj]
+        kk += 1
+
+    for kk in range(left_index, right_index):
+        A[kk] = temp_array[kk - left_index]
+
+    return A
 
 def quickSort(A):
     """ quickSort - front-end for kick-starting the recursive algorithm
