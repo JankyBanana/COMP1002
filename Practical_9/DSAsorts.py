3594020633e3b6e73684cac0152489aa4a84d90a
JankyBanana <luke.lummis@hotmail.com>
2025-05-22 11:48:57 +0800

    Added median of 3 quick sort

--- Practical_9/DSAsorts.py
+++ Practical_9/DSAsorts.py
@@ -35,22 +35,22 @@ def selectionSort(A):
 
 def mergeSort(A):
     left_index = 0
-    right_index = A.size
+    right_index = A.size - 1
     mergeSortRecurse(A, left_index, right_index)
 
 
-def mergeSortRecurse(A, left_index, right_index):
+def mergeSortRecurse(array, left_index, right_index):
     if left_index < right_index:
         mid_index = (left_index + right_index) // 2
 
-        mergeSortRecurse(A, left_index, mid_index)
-        mergeSortRecurse(A, mid_index + 1, right_index)
+        mergeSortRecurse(array, left_index, mid_index)
+        mergeSortRecurse(array, mid_index + 1, right_index)
 
-        merge(A, left_index, mid_index, right_index)
-    return A
+        merge(array, left_index, mid_index, right_index)
+    return array
 
 
-def merge(A, left_index, mid_index, right_index):
+def merge(array, left_index, mid_index, right_index):
     temp_size = right_index - left_index + 1
     temp_array = np.zeros(temp_size)
     ii = left_index
@@ -58,37 +58,37 @@ def merge(A, left_index, mid_index, right_index):
     kk = 0
 
     while ii <= mid_index and jj <= right_index:
-        if A[ii] <= A[jj]:
-            temp_array[kk] = A[ii]
-            ii += 1
+        if array[ii] <= array[jj]:
+            temp_array[kk] = array[ii]
+            ii = ii + 1
         else:
-            temp_array[kk] = A[jj]
-            jj += 1
-        kk += 1
+            temp_array[kk] = array[jj]
+            jj = jj + 1
+        kk = kk + 1
 
-    for ii in range(ii, mid_index):
-        temp_array[kk] = A[ii]
-        kk += 1
+    for ii in range(ii, mid_index + 1):
+        temp_array[kk] = array[ii]
+        kk = kk + 1
 
-    for jj in range(jj, jj + 1):
-        temp_array[kk] = A[jj]
-        kk += 1
+    for jj in range(jj, right_index + 1):
+        temp_array[kk] = array[jj]
+        kk = kk + 1
 
-    for kk in range(left_index, right_index):
-        A[kk] = temp_array[kk - left_index]
+    for kk in range(left_index, right_index + 1):
+        array[kk] = temp_array[kk - left_index]
 
-    return A
+    return array
 
 
 def quickSort(A):
     left_index = 0
-    right_index = A.size
+    right_index = len(A) - 1
     quickSortRecurse(A, left_index, right_index)
 
 
 def quickSortRecurse(array, left_index, right_index):
     if right_index > left_index:  # Making sure array has size
-        pivot_index = (left_index + right_index) // 2  # Middle element selection
+        pivot_index = left_index  # Left element selection
         new_pivot_index = doPartitioning(array, left_index, right_index, pivot_index)
 
         quickSortRecurse(array, left_index, new_pivot_index - 1)  # Sort left partition
@@ -114,3 +114,39 @@ def doPartitioning(array, left_index, right_index, pivot_index):
     array[new_pivot_index] = pivot_value  # Re-placing pivot at end of left array
 
     return new_pivot_index
+
+
+def quickSortMedian3(array):
+    left_index = 0
+    right_index = len(array) - 1
+    quickSortRecurseMedain3(array, left_index, right_index)
+
+
+def quickSortRecurseMedain3(array, left_index, right_index):
+    if right_index > left_index:  # Making sure array has size
+        pivot_index = medianOf3(array, left_index, right_index)  # Median of 3 elements selection
+        new_pivot_index = doPartitioning(array, left_index, right_index, pivot_index)
+
+        quickSortRecurseMedain3(array, left_index, new_pivot_index - 1)  # Sort left partition
+        quickSortRecurseMedain3(array, new_pivot_index + 1, right_index)  # Sort right partition
+    return array
+
+
+def medianOf3(array, low, high):
+    low_index = low
+    high_index = high
+    mid_index = (low_index + high_index) // 2
+
+    low = array[low_index]
+    mid = array[mid_index]
+    high = array[high_index]
+
+    if mid <= low <= high or high <= low <= mid:
+        median_index = low_index
+    elif low <= mid <= high or high <= mid <= low:
+        median_index = mid_index
+    else:
+        median_index = high_index
+
+    print(f"Low: {low}  Mid: {mid}  High: {high}  Median: {array[median_index]}  Median Index: {median_index}")
+    return median_index
--- Practical_9/SortsTestHarness.py
+++ Practical_9/SortsTestHarness.py
@@ -1,33 +1,35 @@
-#**
-#** Testharness to generate various different types of arrays of integers
-#** and then sort them using various sorts.
-#**
-#** Each sort is run REPEATS times, with the first result discarded,
-#** and the last REPEATS-1 runs averaged to give the running time.
-#**
-#** Copied from Practical 1
-#**
-#** Author of java version: Andrew Turpin (andrew@cs.curtin.edu.au)
-#** Date:    August 2004
-#** Modified (java): Patrick Peursum
-#** Date:     Sep 2009
-#** Modified (python): Valerie Maxville
-#** Date:    August 2017
-#** Modified (python): Hayden Richards
-#** Date:    March 2018
-#** Modified (python): Luke Lummis
-#** Date:    May 2025
+# **
+# ** Testharness to generate various different types of arrays of integers
+# ** and then sort them using various sorts.
+# **
+# ** Each sort is run REPEATS times, with the first result discarded,
+# ** and the last REPEATS-1 runs averaged to give the running time.
+# **
+# ** Copied from Practical 1
+# **
+# ** Author of java version: Andrew Turpin (andrew@cs.curtin.edu.au)
+# ** Date:    August 2004
+# ** Modified (java): Patrick Peursum
+# ** Date:     Sep 2009
+# ** Modified (python): Valerie Maxville
+# ** Date:    August 2017
+# ** Modified (python): Hayden Richards
+# ** Date:    March 2018
+# ** Modified (python): Luke Lummis
+# ** Date:    May 2025
 
-import numpy as np
+import random
 import sys
 import timeit
+
+import numpy as np
+
 import DSAsorts
-import random
 
+REPEATS = 3  # No times to run sorts to get mean time
+NEARLY_PERCENT = 0.10  # % of items to move in nearly sorted array
+RANDOM_TIMES = 100  # No times to randomly swap elements in array
 
-REPEATS = 3           #No times to run sorts to get mean time
-NEARLY_PERCENT = 0.10 #% of items to move in nearly sorted array
-RANDOM_TIMES = 100    #No times to randomly swap elements in array
 
 def usage():
     print(" Usage: java TestHarness n xy [xy ...]")
@@ -38,6 +40,7 @@ def usage():
     print("           i - insertion sort")
     print("           s - selection sort")
     print("           q - quicksort")
+    print("           qm3 - median 3 quicksort")
     print("           m - mergesort")
     print("        y is one of")
     print("           a - 1..n ascending")
@@ -45,61 +48,65 @@ def usage():
     print("           r - 1..n in random order")
     print("           n - 1..n nearly sorted (10% moved)")
 
+
 def doSort(n, sortType, arrayType):
-        A = np.arange(1, n+1, 1)   #create array with values from 1 to n
-        
-        if arrayType == 'a':
-            ...
-        elif arrayType =='d':  #convert to descending
-            for i in range(0, int(n/2)):
-                temp = A[i]
-                A[i] = A[n-i-1]
-                A[n-i-1] = temp
-            print("Descending: ", A)
-        elif arrayType == 'r':
-            for i in range(RANDOM_TIMES*n):
-                x = int(random.random()*n)
-                y = int(random.random()*n)
-                temp = A[x]
-                A[x] = A[y]
-                A[y] = temp
-            print("Random: ", A)
-        elif arrayType == 'n':
-            for i in range(int(n*NEARLY_PERCENT/2+1)):
-                x = int(random.random()*n)
-                y = int(random.random()*n)
-                temp = A[x]
-                A[x] = A[y]
-                A[y] = temp
-            print("Nearly sorted: ", A)
-        else:
-            print("Unsupported array type")
+    A = np.arange(1, n + 1, 1)  # create array with values from 1 to n
 
-        if sortType == "b":
-            DSAsorts.bubbleSort(A)
-        elif sortType == "s":
-            DSAsorts.selectionSort(A)
-        elif sortType == "i":
-            DSAsorts.insertionSort(A)
-        elif sortType == "m":
-            DSAsorts.mergeSort(A)
-        elif sortType == "q":
-            DSAsorts.quickSort(A)
-        else:
-            print("Unsupported sort algorithm")
-        print(A)
+    if arrayType == 'a':
+        ...
+    elif arrayType == 'd':  # convert to descending
+        for i in range(0, int(n / 2)):
+            temp = A[i]
+            A[i] = A[n - i - 1]
+            A[n - i - 1] = temp
+        print("Descending: ", A)
+    elif arrayType == 'r':
+        for i in range(RANDOM_TIMES * n):
+            x = int(random.random() * n)
+            y = int(random.random() * n)
+            temp = A[x]
+            A[x] = A[y]
+            A[y] = temp
+        print("Random: ", A)
+    elif arrayType == 'n':
+        for i in range(int(n * NEARLY_PERCENT / 2 + 1)):
+            x = int(random.random() * n)
+            y = int(random.random() * n)
+            temp = A[x]
+            A[x] = A[y]
+            A[y] = temp
+        print("Nearly sorted: ", A)
+    else:
+        print("Unsupported array type")
 
-        for i in range(n-2):
-            if (A[i] > A[i+1]):
-                raise ValueError("Array not in order")
+    if sortType == "b":
+        DSAsorts.bubbleSort(A)
+    elif sortType == "s":
+        DSAsorts.selectionSort(A)
+    elif sortType == "i":
+        DSAsorts.insertionSort(A)
+    elif sortType == "m":
+        DSAsorts.mergeSort(A)
+    elif sortType == "q":
+        DSAsorts.quickSort(A)
+    elif sortType == "qm3":
+        DSAsorts.quickSortMedian3(A)
+    else:
+        print("Unsupported sort algorithm")
+    print(A)
 
-#main program
+    for i in range(n - 2):
+        if (A[i] > A[i + 1]):
+            raise ValueError("Array not in order")
+
+
+# main program
 
 if len(sys.argv) < 3:
     usage()
 else:
     for aa in range(2, len(sys.argv)):
-        
+
         n = int(sys.argv[1])
         sortType = sys.argv[aa][0]
         arrayType = sys.argv[aa][1]
@@ -107,10 +114,10 @@ else:
         runningTotal = 0
 
         for repeat in range(REPEATS):
-             startTime = timeit.default_timer()
-             doSort(n, sortType, arrayType)
-             endTime = timeit.default_timer()
+            startTime = timeit.default_timer()
+            doSort(n, sortType, arrayType)
+            endTime = timeit.default_timer()
 
-             runningTotal += (endTime - startTime)
-    
-        print(sortType + arrayType + " " + str(n) + " " + str((runningTotal/(REPEATS - 1))*1000))
+            runningTotal += (endTime - startTime)
+
+        print(sortType + arrayType + " " + str(n) + " " + str((runningTotal / (REPEATS - 1)) * 1000))
