da0693321fbfa7585f6a2ff1485667fbbf6359ef
lukel <luke.lummis@hotmail.com>
2025-06-07 20:56:00 +0800

    Implemented support for importing a graph via a csv file

--- Assignment/RouteGraph.py
+++ Assignment/RouteGraph.py
@@ -8,8 +8,92 @@
 #
 
 
-from Assignment import DSAGraph
+from Assignment.DSAModules import DSAGraph as g
 import numpy as np
+
+
+def main():
+    user_input = input(f"\nGraph operations listed below. Select using square brackets [...]\n"
+                       f"------------------------\n"
+                       f" Add Vertex             [ADDV <LABEL> <DATA>]\n"
+                       f" Add Edge               [ADDE <SOURCE> <SINK> <WEIGHT>]\n"
+                       f" Delete Vertex          [DELV <LABEL>]\n"
+                       f" Delete Edge            [DELE <SOURCE> <SINK>]\n"
+                       f" Display as List        [DAL]\n"
+                       f" Breadth First Search   [BFS]\n"
+                       f" Depth First Search     [DFS]\n"
+                       f" Import graph           [IMPORT <filename.txt>]\n"
+                       f"------------------------\n"
+                       f"Input: ")
+
+    route_graph = g.DSAGraph()
+    inputs = string_seperator(user_input)
+
+    while 1:
+        try:
+            inputs[0] = inputs[0].upper()
+
+            if inputs[0] == "ADDV":
+                try:
+                    route_graph.add_vertex(inputs[1], inputs[2])
+                except IndexError:
+                    route_graph.add_vertex(inputs[1], None)
+
+            elif inputs[0] == "ADDE":
+                try:
+                    route_graph.add_edge(inputs[1], inputs[2], inputs[3])
+                except IndexError:
+                    raise Exception
+
+            elif inputs[0] == "DELV":
+                route_graph.delete_vertex(inputs[1])
+
+            elif inputs[0] == "DELE":
+                route_graph.delete_edge(inputs[1], inputs[2])
+
+            elif inputs[0] == "DAL":
+                route_graph.display_as_list()
+
+            elif inputs[0] == "BFS":
+                print(route_graph.breadth_first_search())
+
+            elif inputs[0] == "DFS":
+                print(route_graph.depth_first_search())
+
+            elif inputs[0] == "IMPORT":
+                try:
+                    with open(inputs[1], 'r') as graph_file:
+
+                        line = graph_file.readline()
+                        line_idx = 0
+
+                        for i in range(len(line)):
+                            if line[line_idx] != ',' and line[line_idx] != '\n':
+                                route_graph.add_vertex(line[line_idx])
+                                print(f"Adding vertex with label '{line[line_idx]}'")
+                            line_idx += 1
+
+                        line = graph_file.readline().strip('\n')
+
+                        while line:
+                            route_graph.add_edge(line[0], line[1], float(line[3:]))
+                            print(f"Adding edge from '{line[0]}' to '{line[1]}' with weight '{line[3:]}'")
+                            line = graph_file.readline().strip('\n')
+
+                except FileNotFoundError as e:
+                    print(e)
+
+            elif inputs[0] == "EXIT":
+                break
+
+            else:
+                print("Invalid input.")
+
+        except Exception as e:
+            print(e)
+
+        user_input = input("Input: ")
+        inputs = string_seperator(user_input)
 
 
 def string_seperator(input_str):
@@ -21,68 +105,21 @@ def string_seperator(input_str):
             word_count += 1
 
     results = np.full(word_count, None)
-    result_indx = 0
+    result_idx = 0
 
     for idx in range(len(input_str)):
         if input_str[idx] != " ":
             temp_string += input_str[idx]
+
         elif idx > 0 and input_str[idx - 1] != " ":
-            results[result_indx] = temp_string
+            results[result_idx] = temp_string
             temp_string = ""
-            result_indx += 1
+            result_idx += 1
 
     if temp_string:
-        results[result_indx] = temp_string
+        results[result_idx] = temp_string
 
     return results
 
 
-demo = DSAGraph.DSAGraph()
-
-userInput = input(f"\nGraph operations listed below. Select using square brackets [...]\n"
-                  f"------------------------\n"
-                  f" Add Vertex             [ADDV <LABEL> <DATA>]\n"
-                  f" Add Edge               [ADDE <SOURCE> <SINK> <WEIGHT>]\n"
-                  f" Delete Vertex          [DELV <LABEL>]\n"
-                  f" Delete Edge            [DELE <SOURCE> <SINK>]\n"
-                  f" Display as List        [DAL]\n"
-                  f" Breadth First Search   [BFS]\n"
-                  f" Depth First Search     [DFS]\n"
-                  f"------------------------\n"
-                  f"Input: ")
-
-inputs = string_seperator(userInput)
-
-while 1:
-    try:
-        inputs[0] = inputs[0].upper()
-
-        if inputs[0] == "ADDV":
-            try:
-                demo.add_vertex(inputs[1], inputs[2])
-            except IndexError:
-                demo.add_vertex(inputs[1], None)
-        elif inputs[0] == "ADDE":
-            try:
-                demo.add_edge(inputs[1], inputs[2], inputs[3])
-            except IndexError:
-                raise Exception
-        elif inputs[0] == "DELV":
-            demo.delete_vertex(inputs[1])
-        elif inputs[0] == "DELE":
-            demo.deAdlete_edge(inputs[1], inputs[2])
-        elif inputs[0] == "DAL":
-            demo.display_as_list()
-        elif inputs[0] == "BFS":
-            print(demo.breadth_first_search())
-        elif inputs[0] == "DFS":
-            print(demo.depth_first_search())
-        elif inputs[0] == "EXIT":
-            break
-        else:
-            print("Invalid input. Ensure that all commands are capitalised.")
-    except Exception as e:
-        print(e)
-        print("\nCommand provided was either invalid or missing an argument.")
-    userInput = input("Input: ")
-    inputs = string_seperator(userInput)
+main()
